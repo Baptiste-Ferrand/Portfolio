55351a65905e259775d19a3e21fdc5805b5dd59c
Baptiste-Ferrand <baptiste.ferrand@ynov.com>
2026-02-19 16:29:36 +0100

    init project : init shadcn whit some components init taiwing and other stuff

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu'
+
+</script>
+
+<template>
+  <NavigationMenu :viewport="false">
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+          <a href="/">Home</a>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+          <a href="/Projects">Project</a>
+        </NavigationMenuLink>
+      </NavigationMenuItem> 
+      <NavigationMenuItem>
+        <NavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+          <a href="/docs">Contact</a>
+        </NavigationMenuLink>
+      </NavigationMenuItem> 
+      <NavigationMenuItem>
+        <NavigationMenuLink as-child :class="navigationMenuTriggerStyle()">
+          <a href="/docs">Information</a>
+        </NavigationMenuLink>
+      </NavigationMenuItem> 
+    </NavigationMenuList>
+  </NavigationMenu>
+</template>
